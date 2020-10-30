d082ddb3b1b7d9cc71dd76aa68b09420fa6da7e0
clear <fabian.kamp@fu-berlin.de>
2020-10-30 09:31:41 +0100

    Colored Noise added

--- noise/noise.py
+++ noise/noise.py
@@ -1,24 +1,55 @@
 import numpy as np
+import scipy
+import os
+
 class colored_noise(): 
-    """
-    Class to handle noise which is added to model
-    """
-    def __init__(self, params): 
-        self.params = params
-        noise_dict = {1:'white', 2:'pink', 3:'blue'}
-        noise_color = noise_dict[params['nr_color']]
-        noise_lowcut = params['noise_lowcut']
-        noise_highcut = params['noise_highcut']
-        self.csvfile = 'colored_noise/%s_noise_%d-%d.csv' % (noise_color, noise_lowcut, noise_highcut)
-        self.noise_trace = self.load() 
-    
-    def __getattribute__(self, index):
-        return self.noise_trace[index]
+	"""
+	Class to handle noise which is added to model
+	"""
+	def __init__(self, noise_color, low_cut, high_cut, time, n_samples): 
+		self.noise_color = noise_color
+		self.low_cut = low_cut
+		self.hight_cut = high_cut
+		self.fsample = 1000
+		self.time = time
+		self.n_samples = n_samples
+		self.csvfile = os.path.join('noise',f'{noise_color}_noise_{low_cut}-{high_cut}.csv')
+		self.noise_trace = self.load()
+		self.noise_trace = self.noise_trace[:n_samples, :time]
+	
+	def __getitem__(self, index):
+		return self.noise_trace[index]
 
-    def load(self):
-        noise = np.genfromtxt(self.csvfile, delimiter=',')
-        return noise
+	def load(self):
+		noise = np.genfromtxt(self.csvfile, delimiter=',')
+		return noise
 
-    def generate(self):
-        #TODO translate matlab generator into python
-        pass
+	def generate(self):
+		#TODO translate matlab generator into python
+		pass
+	
+	def plot_noise(self):
+		pass
+	
+	def plotNoiseSpectrum(self, ax):
+		"""
+		Plots a Single-Sided Amplitude Spectrum of Noise with Sampling Frequency Fsample
+		"""
+		n = len(self.noise_trace) # length of the Noise
+		k = np.arange(n)
+		T = n/self.fsample
+		frq = k/T # two sides frequency range
+		frq = frq[range(int(n/2))] # one side frequency range
+
+		Y = scipy.fft(Noise) # fft computing and normalization
+		Y = Y[range(int(n/2))]
+		
+		ax.plot(frq,np.abs(Y), alpha = 0.50) # plotting the spectrum		
+		ax.set_xlabel('Frequency (Hz)')
+		#ax.tick_params(labelbottom=False)
+
+		ax.set_ylabel('Power', fontsize='small')
+		ax.yaxis.set_label_position("right")
+		ax.yaxis.tick_right()
+
+		
